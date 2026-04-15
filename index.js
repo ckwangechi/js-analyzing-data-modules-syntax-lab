65c549af0d276cfc00d57db0b2d06f7c84244ecf
Christine Karuga <christinewangechik@gmail.com>
2026-04-15 08:32:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+function combineUsers(...args) {
 
+  const combinedObject = {users: []};
+  for (const arg of args) {
+    if (Array.isArray(arg)) {
+      combinedObject.push(...arg);
+    }
+  }
+
+  CombinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  return {combinedObject};
+}
 
 
 module.exports = {
